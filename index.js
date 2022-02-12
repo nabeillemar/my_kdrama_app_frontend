ff9e4a7b8e06933e91c8350f207e354f70de203e
Nabeil <nabeillemar@gmail.com>
2022-02-12 10:53:10 -0800

    added get fetch request and kdramasMarkup

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-console.log("in index.js");
+const k_dramasIndexPage = "http://localhost:3000/api/v1/k_dramas"
+
+document.addEventListener("DOMContentLoaded", () => {   // We want to listen to the document, we are telling JS to listen to the document and... we want to listen for our DOMContent to be loaded 
+   getKdramas() //bascially the first step the DOMContent Loaded event will do is render the data using this function 
+});
+
+
+
+
+function getKdramas() { // we need create a function of this Get fetch request and add it to the DOMContentLoaded event 
+    fetch(k_dramasIndexPage)
+    .then(response => response.json())
+    .then(kdramas => {
+        kdramas.data.forEach(kdramas => {
+            const kdramasMarkup =` 
+            <div data-id=${kdramas.id}>
+            <img src=${kdramas.attributes.cover_photo} height="450" width="450">
+            <h3>${kdramas.attributes.title}</h3>
+            <p> ${kdramas.attributes.release_year}<p>
+            <p> ${kdramas.attributes.where_to_watch}<p>
+            <p> ${kdramas.attributes.my_rating}<p>
+            <p> ${kdramas.attributes.comment}<p>
+            <p> ${kdramas.attributes.category.name}<p>
+            <p> ${kdramas.attributes.watched}<p>
+            <button data=id${kdramas.id}>edit</button>
+            </div>
+            <br></br>`;
+            document.querySelector('#kdramas-container').innerHTML += kdramasMarkup // finding the kdramas_container in the HTML and saying we want to update that container with all this markup code 
+        })  
+
+    })
+}
+
+
+
+
+/*
+function getKdramas() { // we need create a function of this Get fetch request and add it to the DOMContentLoaded event 
+    fetch(k_dramasIndexPage)
+    .then(response => response.json())
+    .then(kdramas => console.log(kdramas)); //we can still call the json a different name as well 
+
+}
+
+
+
+
+
+/* this way of adding everything to the DOMContentLoaded is not good because it is not DRY 
+document.addEventListener("DOMContentLoaded", () => {   // We want to listen to the document, we are telling JS to listen to the document and... we want to listen for our DOMContent to be loaded 
+    fetch(k_dramasIndexPage) // Once this listens to the document being loaded we want to make a GET request to the index page, fetch returns a promise, in that promise there is a response that we can parse 
+    .then(response => response.json) // this get the response that the fetch gave us through the promise and we convert it to json , we are parsing the response to json 
+    .then(kdramas => { // get access to the json data, we can name it whatever we want but it should make sense, we should get an array of kdramas back 
+        console.log(kdramas); 
+    })
+    // we can add a .catch() here 
+}) //
+*/
