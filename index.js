7b543589c833ba74b9785dadf01db7b2eae20414
Nabeil <nabeillemar@gmail.com>
2022-02-12 15:35:19 -0800

    add formHandler

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const k_dramasIndexPage = "http://localhost:3000/api/v1/k_dramas"
 
 document.addEventListener("DOMContentLoaded", () => {   // We want to listen to the document, we are telling JS to listen to the document and... we want to listen for our DOMContent to be loaded 
-   getKdramas() //bascially the first step the DOMContent Loaded event will do is render the data using this function 
-});
+   console.log("DOM LOADED")
+    getKdramas() //bascially the first step the DOMContent Loaded event will do is render the data using this function 
 
+   const createKdramaForm = document.querySelector("#create-kdrama-form") 
+
+   createKdramaForm.addEventListener("submit", (e) => createFormHandler(e))
+
+})
 
 
 
@@ -11,7 +16,8 @@ function getKdramas() { // we need create a function of this Get fetch request a
     fetch(k_dramasIndexPage)
     .then(response => response.json())
     .then(kdramas => {
-        kdramas.data.forEach(kdramas => {
+        kdramas.data.forEach(kdramas => { // this part is manipulating the data part 
+            //debugger
             const kdramasMarkup =` 
             <div data-id=${kdramas.id}>
             <img src=${kdramas.attributes.cover_photo} height="450" width="450">
@@ -25,11 +31,29 @@ function getKdramas() { // we need create a function of this Get fetch request a
             <button data=id${kdramas.id}>edit</button>
             </div>
             <br></br>`;
+
             document.querySelector('#kdramas-container').innerHTML += kdramasMarkup // finding the kdramas_container in the HTML and saying we want to update that container with all this markup code 
         })  
 
     })
 }
+
+
+function createFormHandler(e) { // not working? what kind of fetch do we want to make? grabbing all the values for our inputs 
+    e.preventDefault()
+    const title = document.querySelector('#title').value // how does this get connected to the form updated ? 
+    const comment = document.querySelector('#comment').value
+    const coverPhoto = document.querySelector('#cover_photo').value
+    const category = document.querySelector('#categories').value
+    const categoryId = parseInt(category) //the category id 
+    postFetch(title, comment, coverPhoto, categoryId)
+}
+
+function postFetch(title, comment, cover_photo, category_id) { // can name whatever we want here but make sure they are in order 
+    console.log();
+
+}
+
 
 
 
